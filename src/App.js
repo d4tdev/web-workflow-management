0766f11ce395263d5024fca0941723a5d33db4d5
DDatj <jsmsj04@gmail.com>
2023-01-04 00:08:54 +0700

    design trello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <nav className="navbar app">App Bar</nav>
+         <nav className="navbar board">Board Bar</nav>
+         <div className="board-columns">
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+            <div className="column">
+               <header>Brainstorm</header>
+               <ul>
+                  <li>
+                     <img src="https://picsum.photos/200/300" alt="random" />
+                     Title: DD4TJ
+                  </li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+                  <li>Hello from the world!</li>
+               </ul>
+               <footer>Add another card</footer>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
