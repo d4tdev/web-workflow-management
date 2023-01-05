6ae39cb012b99be54bc6a6f4c5a40399751bd225
DDatj <jsmsj04@gmail.com>
2023-01-05 14:29:46 +0700

    clean react architecture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Task.scss';
+
+function Task() {
+   return (
+      <li className="task-item">
+         <img src="https://picsum.photos/200/300" alt="random" />
+         Title: DD4TJ
+      </li>
+   );
+}
+
+export default Task;
