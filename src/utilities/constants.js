3e32eb8b26dabcd4c2031d1e36e0e1968ddc2654
Đạt <jsmsj04@gmail.com>
2024-07-17 01:26:41 +0700

    Update constants.js

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,7 +2,7 @@
  * Create a constant object with the given keys and values.
  * on 28-01-2023
  */
-export const API_ROOT = 'https://panicky-yak-waders.cyclic.app';
+export const API_ROOT = 'https://wfmg.onrender.com';
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE';
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM';
